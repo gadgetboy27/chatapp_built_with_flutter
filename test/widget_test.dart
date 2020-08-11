a858ba7bff911af0be78fe9e0861c72f30cb7d5f
Henry-Peti <iamgadgetboy@gmail.com>
2020-08-11 14:16:08 +1200

    fixed error in test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,11 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../lib/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(FriendlychatApp());
+    await tester.pumpWidget(ChatScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
